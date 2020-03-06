3fca8d6bad145d4ddae94140f70acfcf0d712a3e
jay-shah-au3 <shhahjay@gmail.com>
2020-03-07 01:05:11 +0530

    Deploy

--- app.js
+++ app.js
@@ -28,7 +28,10 @@ app.use(
 );
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static("client/build"))
+    app.use(express.static("client/build"));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 // Get Routes
